1716a5ac9226c0ab122b654266ae460243b0764a
Raphael Vigee <raphvigee@gmail.com>
2020-07-22 21:10:59 +0200

    Initial Commit

---
+++ TouchyTests/TouchyTests.swift
@@ -0,0 +1,34 @@
+//
+//  TouchyTests.swift
+//  TouchyTests
+//
+//  Created by Raphaël Vigée on 22/07/2020.
+//  Copyright © 2020 Raphaël Vigée. All rights reserved.
+//
+
+import XCTest
+@testable import Touchy
+
+class TouchyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
