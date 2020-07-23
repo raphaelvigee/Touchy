e54f060188c67f5cd9dc390c44349fe95b19b63c
Raphael Vigee <raphvigee@gmail.com>
2020-07-23 13:45:53 +0200

    first commit

--- Touchy/AppDelegate.swift
+++ Touchy/AppDelegate.swift
@@ -9,31 +9,140 @@
 import Cocoa
 import SwiftUI
 
+private let kBearIdentifier = NSTouchBarItem.Identifier("io.a2.Bear")
+private let ktoggleCSIdentifier = NSTouchBarItem.Identifier("io.a2.ToggleCS")
+private let kPandaIdentifier = NSTouchBarItem.Identifier("io.a2.Panda")
+private let kGroupIdentifier = NSTouchBarItem.Identifier("io.a2.Group")
+
+
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSTouchBarDelegate {
+    
+    
+    private var hideControlStrip = false
+    private var statusItem: NSStatusItem?
 
-    var window: NSWindow!
+    private var _groupTouchBar: NSTouchBar?
+    private var groupTouchBar: NSTouchBar? {
+        if _groupTouchBar == nil {
+            let groupTouchBar = NSTouchBar()
+            groupTouchBar.defaultItemIdentifiers = [ktoggleCSIdentifier, kBearIdentifier, kPandaIdentifier]
+            groupTouchBar.delegate = self
+            _groupTouchBar = groupTouchBar
+        }
 
+        return _groupTouchBar
+    }
+    private var isVisible: Bool {
+        if groupTouchBar == nil {
+            return false
+        }
+        
+        return groupTouchBar?.isVisible ?? false
+    }
+
+    @objc func bear(_ sender: Any?) {
+        print("Bear")
+    }
+
+    func touchBar(
+        _ touchBar: NSTouchBar,
+        makeItemForIdentifier identifier: NSTouchBarItem.Identifier
+    ) -> NSTouchBarItem? {
+        if identifier.rawValue == kBearIdentifier.rawValue {
+            let bear = NSCustomTouchBarItem(identifier: kBearIdentifier)
+            bear.view = NSButton(title: "BEAR", target: self, action: #selector(bear(_:)))
+            return bear
+        } else if identifier.rawValue == kPandaIdentifier.rawValue {
+            let panda = NSCustomTouchBarItem(identifier: kPandaIdentifier)
+            panda.view = NSButton(title: "PANDA", target: self, action: #selector(present))
+            return panda
+        }else if identifier.rawValue == kGroupIdentifier.rawValue {
+            print("group")
+            return nil
+        }else if identifier.rawValue == ktoggleCSIdentifier.rawValue {
+            let panda = NSCustomTouchBarItem(identifier: ktoggleCSIdentifier)
+            panda.view = NSButton(title: "CS", target: self, action: #selector(toggleCS))
+            return panda
+        } else {
+            return nil
+        }
+    }
+    
+    @objc func toggleCS() {
+        hideControlStrip = !hideControlStrip
+        dismiss()
+        present()
+        showControlStripIcon()
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem?.button?.title = "🚀"
+        statusItem?.button?.action = #selector(toggle)
+    
+        present()
+        showControlStripIcon()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    
+    
+    
+    
+    func showControlStripIcon() {
+        DFRSystemModalShowsCloseBoxWhenFrontMost(false)
+    
+        let panda = NSCustomTouchBarItem(identifier: kGroupIdentifier)
+        panda.view = NSButton(title: "MY", target: self, action: #selector(toggle))
+        
+        NSTouchBarItem.removeSystemTrayItem(panda)
+        NSTouchBarItem.addSystemTrayItem(panda)
+    }
+    
+    @objc func toggle() {
+        if self.isVisible {
+            self.minimize()
+        }else {
+            self.present()
+        }
+    }
+    
+    @objc func dismiss() {
+        if #available (macOS 10.14, *) {
+            NSTouchBar.dismissSystemModalTouchBar(groupTouchBar)
+        } else {
+            NSTouchBar.dismissSystemModalFunctionBar(groupTouchBar)
+        }
+    }
+    
+    @objc func minimize() {
+        if #available (macOS 10.14, *) {
+            NSTouchBar.minimizeSystemModalTouchBar(groupTouchBar)
+        } else {
+            NSTouchBar.minimizeSystemModalFunctionBar(groupTouchBar)
+        }
+    }
+    
+    @objc func present() {
+        self.presentFromSystemTrayItem()
+    }
+    
+    @objc func presentFromSystemTrayItem() {
+        let placement: Int64 = hideControlStrip ? 1 : 0
+        self.presentWithPlacement(placement: placement)
+    }
+    
+    private func presentWithPlacement(placement: Int64) {
+        if #available (macOS 10.14, *) {
+            NSTouchBar.presentSystemModalTouchBar(groupTouchBar, placement: placement, systemTrayItemIdentifier: kGroupIdentifier)
+        } else {
+            NSTouchBar.presentSystemModalFunctionBar(groupTouchBar, placement: placement, systemTrayItemIdentifier: kGroupIdentifier)
+        }
+    }
 
 }
 
--- Touchy/ContentView.swift
+++ Touchy/ContentView.swift
@@ -20,4 +20,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+
 }
